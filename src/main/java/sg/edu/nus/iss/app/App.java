ff679d3c5dc6a8beb4659a2daa70a3704b11a6ca
KaiX9 <koh_kaixiang_8@hotmail.com>
2023-02-10 14:28:23 +0800

    day08_workshop

--- src/main/java/sg/edu/nus/iss/app/App.java
+++ src/main/java/sg/edu/nus/iss/app/App.java
@@ -1,15 +1,19 @@
 package sg.edu.nus.iss.app;
 
+import java.io.Console;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.util.List;
 
 public final class App {
     private App() {
     }
+
+    public static String dirName = "day08data";
+    public static String fileName = "data.txt";
+    public static String dirFileName = dirName + File.separator + fileName;
 
     public static void main(String[] args) throws IOException {
 
@@ -19,10 +23,6 @@ public final class App {
                 System.exit(0);
             }
         }
-
-        String dirName = "day08data";
-        String fileName = "data.txt";
-        String dirFileName = dirName + File.separator + fileName;
 
         FileService fs = new FileService();
         Boolean directoryCreated = fs.createDirectory(dirName);
@@ -41,13 +41,42 @@ public final class App {
             System.out.println("file already exists:" + fileName);
         }
 
+        Console con = System.console();
+        String conInput = "";
+
+        while (!conInput.equalsIgnoreCase("Q")) {
+            displayMenu();
+            conInput = con.readLine("Enter your selection:");
+
+            switch (conInput) {
+                case "1":
+                    CSVExample();
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "Q":
+                case "q":
+                    message("Bye... bye...");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+
+    public static void CSVExample() throws IOException {
         try {
             // FileOutputStream fos = new FileOutputStream(dirFileName);
 
             // for (int i = 0; i < 20; i++) {
-            //     // writing string in char array
-            //     fos.write(Integer.toString(i).getBytes());
-            //     fos.write('\n');
+            // // writing string in char array
+            // fos.write(Integer.toString(i).getBytes());
+            // fos.write('\n');
             // }
 
             // clear the outputstream
@@ -68,7 +97,19 @@ public final class App {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+    }
 
+    public static void displayMenu() {
+        message("Welcome to My App");
+        message("=============================");
+        message("1. CSV Read and Write Test");
+        message("2. Read Idioms File");
+        message("3. Pick an idiom randomly");
+        message("4. List all idioms");
+        message("Q  Quit the program");
+    }
 
+    public static void message(String line) {
+        System.out.println(line);
     }
 }
